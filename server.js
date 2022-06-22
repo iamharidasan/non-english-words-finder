4aaea2fd19cf3a05f854aba7a547d41c99b60afe
Haridasan Shanmugam <hdasshp@gmail.com>
2022-06-22 22:40:41 +0530

    Heroku deployment config added

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const path = require("path")
 const app = express()
 const cors = require("cors")
 
@@ -8,6 +9,16 @@ app.use(express.json())
 app.disable("x-powered-by")
 
 app.use("/api/words", require("./words/words"))
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  //Set Static folder
+  app.use(express.static("client/build"))
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  })
+}
 
 const PORT = process.env.BPORT || 5000
 app.listen(PORT, () => {
